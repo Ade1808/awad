2f430800e81a84a0a0f9dfa1da726d9c8fc12c4a
esmali <a.rasaki.07@gmail.com>
2019-02-20 22:36:53 +0000

    CHeck

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 =>  587,
   :authentication       =>  :plain,
   :user_name            =>  'apikey',
-  :password             =>  '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',
+  :password             =>  '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',
   :domain               =>  'heroku.com',
   :enable_starttls_auto  =>  true
 }
